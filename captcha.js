7920a974c42414c61526b1142e98de5e3435af59
Nikita <zininnikita309@gmail.com>
2022-06-24 19:39:52 +0900

    init

---
+++ captcha.js
@@ -0,0 +1,26 @@
+function createCaptcha(
+  options = {
+    alphabet: [],
+    length: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
+  },
+) {
+  const alphabet = options.alphabet;
+  const length = options.length;
+
+  function getArrayRandNumbers() {
+    const max = alphabet.length - 1;
+
+    const getRandNumbers = () => alphabet[Math.floor(Math.random() * max)];
+
+    const arrayRandNumbers = new Array(length).fill(0).map(getRandNumbers);
+    return arrayRandNumbers;
+  }
+
+  const randomArray = getArrayRandNumbers();
+
+  const captchaElement = document.createElement('div');
+  captchaElement.className = 'captcha';
+  document.body.appendChild(captchaElement);
+
+  randomArray.forEach((symbol) => (captchaElement.innerHTML += symbol));
+}
