9ca2fb1cea87ae96caa73c14a8e3429a4ae9eff5
Nikita <zininnikita309@gmail.com>
2022-10-23 14:28:26 +0900

    v2.0.0

--- captcha.js
+++ captcha.js
@@ -1,4 +1,4 @@
-export default function createCaptcha(
+function createCaptcha(
   options = {
     alphabet: [],
     length: 4,
@@ -6,21 +6,10 @@ export default function createCaptcha(
 ) {
   const alphabet = options.alphabet;
   const length = options.length;
+  const max = alphabet.length - 1;
 
-  function getArrayRandNumbers() {
-    const max = alphabet.length - 1;
+  const getRandNumbers = () => alphabet[Math.floor(Math.random() * max)];
+  const captcha = new Array(length).fill(0).map(getRandNumbers);
 
-    const getRandNumbers = () => alphabet[Math.floor(Math.random() * max)];
-
-    const arrayRandNumbers = new Array(length).fill(0).map(getRandNumbers);
-    return arrayRandNumbers;
-  }
-
-  const randomArray = getArrayRandNumbers();
-
-  const captchaElement = document.createElement("div");
-  captchaElement.className = "captcha";
-  document.body.appendChild(captchaElement);
-
-  randomArray.forEach((symbol) => (captchaElement.innerHTML += symbol));
+  return captcha.join("");
 }
